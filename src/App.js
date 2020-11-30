f5b02492f22b06df21142a2e3abc6e20ba5324f1
Anu Singh <anusingh1883656@gmail.com>
2020-11-30 20:35:12 +0530

    Pagination and Search components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import React, { useState } from 'react'
 import useFetchJobs from "./useFetch"
 import { Container } from "react-bootstrap"
 import Job from "./job"
+import JobsPagination from './JobsPagination'
+import SearchForm from './SearchForm'
 
 
 function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
-  const { jobs, error, loading } = useFetchJobs(params, page)
+  const { jobs, error, loading, hasNextPage } = useFetchJobs(params, page)
 
+
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
 
   return (
     <Container className="my-4">
       <h1 className="mb-4">Github jobs</h1>
-      {loading && <h1>Loading ...</h1>}
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      { loading && <h1>Loading ...</h1>}
       {error && <h1>Error, Try Refereshing.</h1>}
       {jobs.map(job => {
         return <Job key={job.id} job={job} />
       }
       )}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   )
 }
